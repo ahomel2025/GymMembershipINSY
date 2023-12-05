3ae8296f9f6be4620de00bb0852388d650d19528
Brian Gertsberg <brian.gertsberg@marquette.edu>
2023-12-05 14:46:00 -0600

    Provides name dates and membership type for each customer going to the gym.

---
+++ GymMembershipINSY/model/gymMembership.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GymMembershipINSY.model;  
+    internal class GymMembership  
+    {
+  
+     public string MemberName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+        public string MembershipType { get; set; }
+
+       
+
+        public GymMembership(string name, DateTime startDate, DateTime endDate, string membershipType)
+        {
+            MemberName = name;
+            StartDate = startDate;
+            EndDate = endDate;
+            IsActive = true;
+            MembershipType = membershipType;
+        }
+
+        public void RenewMembership(int months)
+        {
+            EndDate = EndDate.AddMonths(months);
+            IsActive = true;
+            Console.WriteLine($"{MemberName}'s {MembershipType} membership has been renewed until {EndDate.ToShortDateString()}");
+        }
+
+        public void CheckMembershipStatus()
+        {
+            if (IsActive)
+            {
+                Console.WriteLine($"{MemberName}'s {MembershipType} membership is active until {EndDate.ToShortDateString()}");
+            }
+            else
+            {
+                Console.WriteLine($"{MemberName}'s {MembershipType} membership has expired");
+            }
+        }
+    }
+

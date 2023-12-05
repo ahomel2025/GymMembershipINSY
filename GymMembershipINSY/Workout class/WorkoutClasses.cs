f5558176a43ac2e4a8a9556a679d6c52223b323e
Meghan Morsch <meghan.morsch@marquette.edu>
2023-12-05 14:53:41 -0600

    Adds memberships for john and jane for certain amount of time and adds trainers/displays them

--- GymMembershipINSY/Workout class/WorkoutClasses.cs
+++ GymMembershipINSY/Workout class/WorkoutClasses.cs
@@ -8,6 +8,20 @@ namespace GymMembershipINSY.Workout_class
 {
     internal class WorkoutClasses
     {
+        Gym gym = new Gym(); //creating new gym
 
+        Gym.CreateMembership("John Doe", DateTime.Now, DateTime.Now.AddMonths(6), "Standard"); //creating membership for john doe for 6 months and standard
+        gym.CreateMembership("Jane Smith", DateTime.Now, DateTime.Now.AddMonths(3), "Premium"); //creating memberhsip for Jane smith now for 3 months and premium
+
+        gym.CheckMembershipStatus("John Doe"); //checking status of john does membership
+        gym.CheckMembershipStatus("Jane Smith"); //checking status of jane smitch membership
+
+        gym.RenewMembership("John Doe", 3); //renewing john doe membership for 3 months
+        gym.CheckMembershipStatus("John Doe"); //checking john does status
+
+        gym.AddTrainer("Mike", "Weightlifting"); //adding trainer Mike who does weighlifting
+        gym.AddTrainer("Sarah", "Yoga"); //adding trainer sarah who teaches yoga
+
+        gym.DisplayTrainers(); //displays trainers
     }
 }

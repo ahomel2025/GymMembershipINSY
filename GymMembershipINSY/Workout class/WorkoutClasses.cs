c1c520202fcdb5960bcb99eb79bb606c1446b578
briangertsberg <151680876+briangertsberg@users.noreply.github.com>
2023-12-09 11:48:05 -0600

    Update WorkoutClasses.cs
    We're establishing the attributes for a workout class

--- GymMembershipINSY/Workout class/WorkoutClasses.cs
+++ GymMembershipINSY/Workout class/WorkoutClasses.cs
@@ -6,22 +6,17 @@ using System.Threading.Tasks;
 
 namespace GymMembershipINSY.Workout_class
 {
-    internal class WorkoutClasses
+    public class WorkoutClasses
     {
-        Gym gym = new Gym(); //creating new gym
-
-        Gym.CreateMembership("John Doe", DateTime.Now, DateTime.Now.AddMonths(6), "Standard"); //creating membership for john doe for 6 months and standard
-        gym.CreateMembership("Jane Smith", DateTime.Now, DateTime.Now.AddMonths(3), "Premium"); //creating memberhsip for Jane smith now for 3 months and premium
-
-        gym.CheckMembershipStatus("John Doe"); //checking status of john does membership
-        gym.CheckMembershipStatus("Jane Smith"); //checking status of jane smitch membership
-
-        gym.RenewMembership("John Doe", 3); //renewing john doe membership for 3 months
-        gym.CheckMembershipStatus("John Doe"); //checking john does status
-
-        gym.AddTrainer("Mike", "Weightlifting"); //adding trainer Mike who does weighlifting
-        gym.AddTrainer("Sarah", "Yoga"); //adding trainer sarah who teaches yoga
-
-        gym.DisplayTrainers(); //displays trainers
+       private static int autoIncrement;
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+        
+        public WorkoutClasses()
+        {
+            autoIncrement++;
+            Id = autoIncrement;
+        }
+         
     }
 }

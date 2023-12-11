d7841fb45241e2eb592bd35645363868e1bc619f
meghanmorsch <151680878+meghanmorsch@users.noreply.github.com>
2023-12-11 17:41:48 -0600

    Update Gym.cs
    added comments

--- GymMembershipINSY/model/Gym.cs
+++ GymMembershipINSY/model/Gym.cs
@@ -7,26 +7,26 @@ using System.Threading.Tasks;
 
 namespace GymMembershipINSY.model;
 
-public class Members
+public class Members //class for members
     {
-        public List<Member> members { get; set; }
+        public List<Member> members { get; set; } //get and setter method for a list of members
 
-        public Members()
+        public Members() //property to create list for members 
         {
-            members = new List<Member>();
+            members = new List<Member>(); //creation of new list
         }
 
-        public Member Authenticate(string username, string password)
+        public Member Authenticate(string username, string password) //creation of string username and string password
         {
-            var m = members.Where(o => (o.Username == username) && (o.Password == password));
+            var m = members.Where(o => (o.Username == username) && (o.Password == password)); //variable to label what username and password mean
 
-            if(m.Count() > 0)
+            if(m.Count() > 0) //if statement to return members first name if given a member
             {
-                return m.First();
+                return m.First(); //returning first name
             }
-            else
+            else //else statement
             {
-                return null;
+                return null; //return null if no amount of members is given
             }
         }
     }
